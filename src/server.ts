9e0e2d6939d79fe1dfaccb33e3d23782e3af17f9
lucastimoteo <95625902+LucasDoGit@users.noreply.github.com>
2025-02-26 23:52:55 -0300

    chore(deploy: criação das pastas tmp e dist para deploy na vercel

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import 'express-async-errors'
 import cors from 'cors'
 import path from 'path'
 
-import { router } from './routes'
-import fileUpload from 'express-fileupload'
+import { router } from './routes';
+import fileUpload from 'express-fileupload';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors())
 app.use(fileUpload({
@@ -17,11 +18,11 @@ app.use(router);
 app.use(
     '/files',
     express.static(path.resolve(__dirname, '..', 'tmp'))
-)
+);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if(err instanceof Error){
-        res.status(400).json({
+        return res.status(400).json({
             error: err.message
         })
     }
